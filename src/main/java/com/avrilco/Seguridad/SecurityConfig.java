7d0ec10b00a30b95f2e26b690d3d094f0ededebc
Mario Castro <mkstro.3@gmail.com>
2017-12-28 21:52:25 -0600

    F-0003 excepcion corregida

--- src/main/java/com/avrilco/Seguridad/SecurityConfig.java
+++ src/main/java/com/avrilco/Seguridad/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.*;
+//import org.springframework.security.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
